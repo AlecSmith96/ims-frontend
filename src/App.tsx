60ba73f4ff684987aa70c9b43a7f4b6ea8513d4d
Alec <rcsmith.alec@gmail.com>
2021-01-21 23:21:54 +0000

    set navbar links to only appear when user is authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -24,13 +24,18 @@ function App() {
           </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Nav className="mr-auto">
-            <Nav.Link className="text-white" href="/dashboard">Dashboard</Nav.Link>
-            <Nav.Link className="text-white" href="/lookup">Product Lookup</Nav.Link>
-            <Nav.Link className="text-white" href="/lookup">Customer Orders</Nav.Link>
-            <Nav.Link className="text-white" href="/lookup">Supplier Orders</Nav.Link>
-            <Nav.Link className="text-white" href="/lookup">Reports</Nav.Link>
-          </Nav>
+          
+            {localStorage.getItem('authenticated') === null ? 
+            <div/>
+            : 
+            <Nav className="mr-auto">
+              <Nav.Link className="text-white" href="/dashboard">Dashboard</Nav.Link>
+              <Nav.Link className="text-white" href="/lookup">Product Lookup</Nav.Link>
+              <Nav.Link className="text-white" href="/lookup">Customer Orders</Nav.Link>
+              <Nav.Link className="text-white" href="/lookup">Supplier Orders</Nav.Link>
+              <Nav.Link className="text-white" href="/lookup">Reports</Nav.Link>
+              </Nav>
+            }
         {localStorage.getItem('user_name') === null ?
           <Navbar.Text  className='text-white'>Not signed in: <Button  className='text-white' href='/login' variant="outline-info">Sign In</Button></Navbar.Text> :
           <Navbar.Text  className='text-white'>Signed in as: {localStorage.getItem('user_name')} <Button onClick={() => localStorage.clear()} className='text-white' href='/login' variant="outline-info">Sign Out</Button></Navbar.Text> }
