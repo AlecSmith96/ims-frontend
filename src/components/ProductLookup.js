39c46b740ede60962d1ef88bcbdf3343a7eb26f1
Alec <rcsmith.alec@gmail.com>
2021-02-06 16:51:36 +0000

    Implemented findBySku for ProductLookup

--- src/components/ProductLookup.js
+++ src/components/ProductLookup.js
@@ -4,6 +4,7 @@ import { Button } from 'react-bootstrap';
 
 const ProductLookup = (props) => {
     const history = useHistory();
+    const [sku, setSku] = useState('');
     const [products, setProducts] = useState([{}]);
     const [searchTerm, setSearchTerm] = React.useState('');
     const [searchResults, setSearchResults] = React.useState([{}]);
@@ -22,7 +23,7 @@ const ProductLookup = (props) => {
             'Authorization': `bearer ${localStorage.getItem('access_token')}`
             }})
             .then(res => res.json())
-            .then((data) => {setProducts(data)})
+            .then((data) => {setProducts(data)}) 
             .catch(console.error());
     }, []);
 
@@ -31,11 +32,26 @@ const ProductLookup = (props) => {
         history.push({pathname:`/product/${product.id}`, state: product});
     }
 
+    const handleSkuChange = (event) => {
+        setSku(event.target.value);
+    }
+
+    const searchBySku = () => {
+        const product = products.find(product => product.sku.toString() === sku)
+
+        if (product === undefined) {
+            alert('No results for sku.');
+        }
+        else {
+            history.push({pathname:`/product/${product.id}`, state: product});
+        }
+    } 
+
     return (
         <div>
             <center><h2>Product Lookup</h2></center>
-            <form className="container form-inline md-form mr-auto col-md-10">
-                <input className="form-control mr-sm-2" type="text" placeholder="Enter Sku" aria-label="Search"/>
+            <form className="container form-inline md-form mr-auto col-md-10" onSubmit={searchBySku}>
+                <input className="form-control mr-sm-2" type="text" placeholder="Enter Sku" aria-label="Search" value={sku} onChange={handleSkuChange}/>
                 <Button className="btn btn-rounded btn-sm my-0" variant="outline-info" type="submit">Search</Button>
                 <label htmlFor="name" className="col-sm-2 col-form-label text-middle">Or </label>
             </form>
