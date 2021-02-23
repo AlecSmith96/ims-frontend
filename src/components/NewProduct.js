ac1fd870d755b5e74920b6748b2eb9c722d9a337
Alec <rcsmith.alec@gmail.com>
2021-02-23 14:46:45 +0000

    Added sending POST request to add new product to database

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -4,6 +4,13 @@ import {Modal, Button} from 'react-bootstrap';
 
 const NewProduct = (props) => {
     const [supplierNames, setSupplierNames] = useState(['']);
+    const [name, setName] = useState('');
+    const [sku, setSku] = useState('');
+    const [startingInventory, setStartingInventory] = useState(0);
+    const [reorderThreshold, setReorderThreshold] = useState(0);
+    const [price, setPrice] = useState(0.00);
+    const [reorderAmount, setReorderAmount] = useState(0);
+    const [supplierName, setSupplierName] = useState('');
 
     useEffect(() => {
         //get supplier names
@@ -17,7 +24,25 @@ const NewProduct = (props) => {
         .catch(console.error());
     }, [])
 
-    
+    /**
+     * Create JSON for new product entity and send POST request to add it to the
+     *  database.
+     */
+    function submitNewProduct() {
+        const productJson = `{"name": "${name}", "sku": "${sku}", "inventoryOnHand": "${startingInventory}",`+
+            `"reorderThreshold": "${reorderThreshold}", "price": "${price}", "reorderQuantity": "${reorderAmount}",`+
+            `"supplierName": "${supplierName}"}`;
+
+        fetch('http://localhost:8080/api/product/add', {
+            method: 'POST',
+            headers: {'Authorization': `bearer ${localStorage.getItem('access_token')}`,
+                    'Content-Type': 'application/json'},
+            body: productJson
+        })
+        .then(res => res.json())
+        .then((data) => {setSupplierNames(data)})
+        .catch(console.error());
+    }
 
     return (
         <Modal show={props.showModal} onHide={() => props.setModal(false)}>
@@ -25,49 +50,49 @@ const NewProduct = (props) => {
                 <Modal.Title className="">Add a new Product for trading</Modal.Title>
             </Modal.Header>
 
-            <form /*onSubmit={() => submitSupplierOrder()}*/>
+            <form onSubmit={() => submitNewProduct()}>
             <Modal.Body>
                     <p className="lead">Product Details:</p>
                     <div className="form-group row">
                         <label for="inputName" className="col-form-label ml-3">Name</label>
                         <div className="col-sm-9">
-                            <input className="form-control" type="text" placeholder="Pepsi Max 330ml" required/>
+                            <input className="form-control" type="text" placeholder="Pepsi Max 330ml" onChange={(e) => {setName(e.target.value)}} required/>
                         </div>
                     </div>
                     <div className="form-group row">
                         <label for="inputSku" className="col-form-label ml-3">Sku</label>
                         <div className="col-sm-9">
-                            <input className="form-control" type="text" placeholder="12345678" required/>
+                            <input className="form-control" type="text" placeholder="12345678" onChange={(e) => {setSku(e.target.value)}} required/>
                         </div>
                     </div>
                     <div className="form-group row">
                         <label for="inputStartingInventory" className="col-form-label ml-3">Starting Inventory</label>
                         <div className="col-sm-9">
-                            <input className="form-control" type="number" min="1" step="any" placeholder="100" required/>
+                            <input className="form-control" type="number" min="1" step="any" placeholder="100" onChange={(e) => {setStartingInventory(e.target.value)}} required/>
                         </div>
                     </div>
                     <div className="form-group row">
                         <label for="inputReorderThreshold" className="col-form-label ml-3">Reorder Threshold</label>
                         <div className="col-sm-9">
-                            <input className="form-control" type="number" min="1" step="any" placeholder="20" required/>
+                            <input className="form-control" type="number" min="1" step="any" placeholder="20" onChange={(e) => {setReorderThreshold(e.target.value)}} required/>
                         </div>
                     </div>
                     <div className="form-group row">
                         <label for="inputPrice" className="col-form-label ml-3">Price</label>
                         <div className="col-sm-9">
-                            <input className="form-control" type="number" min="0.01" step="any" placeholder="1.00" required/>
+                            <input className="form-control" type="number" min="0.01" step="any" placeholder="1.00" onChange={(e) => {setPrice(e.target.value)}} required/>
                         </div>
                     </div>
                     <div className="form-group row">
                         <label for="inputReorderAmount" className="col-form-label ml-3">Reorder Amount</label>
                         <div className="col-sm-9">
-                            <input className="form-control" type="number" min="1" step="any" placeholder="100" required/>
+                            <input className="form-control" type="number" min="1" step="any" placeholder="100" onChange={(e) => {setReorderAmount(e.target.value)}} required/>
                         </div>
                     </div>
                     <div className="form-group row">
                         <label for="inputSupplier" className="col-form-label ml-3">Supplier</label>
                         <div className="col-sm-10">
-                            <select id="inputSupplier" className="form-control" required>
+                            <select id="inputSupplier" className="form-control" onChange={(e) => {setSupplierName(e.target.value)}} required>
                                 <option defaultValue>Choose...</option>
                                 {
                                     supplierNames.map((name) => <option>{name}</option>)
