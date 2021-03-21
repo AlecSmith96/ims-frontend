5890ac5e0e3495907c7e6486d88f94a62b9589d8
Alec <rcsmith.alec@gmail.com>
2021-03-21 13:55:25 +0000

    updated form styling

--- src/components/StockMovementComponent.js
+++ src/components/StockMovementComponent.js
@@ -19,15 +19,22 @@ const StockMovementComponent = () => {
             <p className="lead mt-5"> View all Stock Movemenets for a given time period </p>
 
             <form className="container col-md-10" onSubmit={() => submitStockMovementRequest()}>
-                <div className="row col-md-10">
-                    <p className="lead">From: </p>
-                    <input type="date" id="start" name="trip-start" placeholder="2020-07-22" min="2020-01-01" max="2024-12-31" onChange={(e) => setStartDate(e.target.value)}></input>
+                <div className="container col-md-6">
+                    <div className="form-group row">
+                        <p className="lead col-form-label">From: </p>
+                        <div className="col-sm-10">
+                            <input className="form-control" type="date" id="start" name="trip-start" placeholder="2020-07-22" min="2020-01-01" max="2024-12-31" onChange={(e) => setStartDate(e.target.value)}></input>
+                        </div>
+                    </div>
+                    <div className="form-group row mt-1">
+                        <p className="lead col-form-label pr-3">To: </p>
+                        <div className="col-sm-10">
+                            <input className="form-control" type="date" id="start" name="trip-start" min="2020-01-01" max="2024-12-31" onChange={(e) => setEndDate(e.target.value)}></input>
+                        </div>
+                    </div>
                 </div>
-                <div className="row col-md-10">
-                    <p className="lead">To: </p>
-                    <input type="date" id="start" name="trip-start" min="2020-01-01" max="2024-12-31" onChange={(e) => setEndDate(e.target.value)}></input>
-                </div>
-                <input className="btn btn-info" type="submit" value="Submit Report Request" />
+                
+                <input className="btn btn-info mt-4" type="submit" value="Submit Report Request" />
             </form>
         </div>
     )
