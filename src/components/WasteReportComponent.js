6d9cd8939bce17b84ac4a614fd94b8defd96a051
Alec <rcsmith.alec@gmail.com>
2021-03-22 13:44:39 +0000

    Implemented Daily Waste form

--- src/components/WasteReportComponent.js
+++ src/components/WasteReportComponent.js
@@ -2,35 +2,78 @@ import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
 const WasteReportComponent = () => {
-    const [products, setProducts] = useState([{}]);
-
-    function addProductField(products) {
-        setProducts(...products, '');
-    }
+    const [wasteRecords, setWasteRecords] = useState([{sku: '', reason: '', quantity: 0}]);
 
     function handleSubmit() {
-
+        const records = `${JSON.stringify(wasteRecords)}`;
+        console.log(records);
+        
+        fetch('http://localhost:8080/api/reports/waste', {
+                method: 'POST',
+                headers: {'Authorization': `bearer ${localStorage.getItem('access_token')}`,
+                            'Content-Type': 'application/json'},
+                body: records
+            })
+            .catch(console.error());
     }
+
+    function addWasteRecord() {
+        const values = [...wasteRecords];
+        values.push({sku: '', reason: '', quantity: 0})
+        setWasteRecords(values);
+    }
+
+    function handleSkuChange(i, event) {
+        const values = [...wasteRecords];
+        values[i].sku = event.target.value;
+        setWasteRecords(values);
+    }
+
+    function handleReasonChange(i, event) {
+        const values = [...wasteRecords];
+        values[i].reason = event.target.value;
+        setWasteRecords(values);
+    }
+
+    function handleQuantityChange(i, event) {
+        const values = [...wasteRecords];
+        values[i].quantity = event.target.value;
+        setWasteRecords(values);
+    }
+
+    function handleRemove(index) {
+        const values = [...wasteRecords];
+        values.splice(index, 1);
+        setWasteRecords(values);
+      }
 
     return (
         <div>
             <h5>Create new waste report</h5>
-            <div id="allergiesDiv" className="form-group row  col-sm-12">
-                        <h1 className="text-info h4 float-left">Products</h1>
-                        <Button variant="outline-info float-right" onClick={(product) => addProductField(product)}>Add Product</Button>
-                    </div>
+            <p className="lead">Enter the Sku, reason for wastage and quantity of stock for each item.</p>
             <form onSubmit={handleSubmit}>
                 {
-                        products.map((product, index) => {
+                        wasteRecords.map((record, index) => {
                             return (
-                                <div key={index} className="form-group row col-sm-12 input-group">
-                                    <input type="text" className="form-control" value={product} onChange={(newAllergy) => this.addAllergyValue(newAllergy, index)} placeholder="New Allergy"/>
-                                    <button className="input-group-append form-control text-danger col-sm-2" onClick={() => this.removeAllergyField(index)}>Remove</button>
+                                <div key={index} className="form-group  col-sm-12 input-group">
+                                    <input id="sku" type="text" className="form-control" value={record.sku} placeholder="Enter Sku..." onChange={e => handleSkuChange(index, e)}/>
+                                    <select id="reason" className="form-control" onChange={(e) => handleReasonChange(index, e)} required>
+                                        <option defaultValue>Choose...</option>
+                                        <option >DAMAGED</option>
+                                        <option >EXPIRED</option>
+                                    </select>
+                                    <input id="quantity" type="number" className="form-control" value={record.quantity} placeholder="Enter Quantity" onChange={(e) => handleQuantityChange(index, e)} required/>
+                                    <button className="input-group-append form-control text-danger col-sm-2" onClick={() => handleRemove(index)}>Remove</button>
                                 </div>
                             )
                         })
                 }
+                <div id="allergiesDiv" className="col-sm-12">
+                    <Button variant="outline-info float-left" onClick={() => addWasteRecord()}>Add Waste record</Button>
+                    <input className="btn btn-info float-right" type="submit" value="Submit Waste Report" />
+                </div>
             </form>
+            
         </div>
     )
 }
