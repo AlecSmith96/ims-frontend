e1fc9df3f21c20e5f893911dcc9ac0782ea4328e
Alec <rcsmith.alec@gmail.com>
2021-03-27 10:39:22 +0000

    Got Low Stock table to display products low on stock

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,6 +10,7 @@ import NewPassword from './NewPassword';
 const Dashboard = (props) => {
     const {state} = useLocation();
     const history = useHistory();
+    const [lowProductStock, setLowProductStock] = useState([{}]);
     const [showAddUserModal, setShowAddUserModal] = useState(false);
     const [showEditUserModal, setShowEditUserModal] = useState(false);
     const [showNewPasswordModal, setShowNewPasswordModal] = useState(false);
@@ -22,48 +23,83 @@ const Dashboard = (props) => {
             localStorage.setItem("authorities", userObj.authorities);
             props.setStatus('logged in');
         }
-        })
+
+        fetch('http://localhost:8080/api/products/low-stock', {
+            method: 'GET',
+            headers: {
+            'Authorization': `bearer ${localStorage.getItem('access_token')}`
+            }})
+        .then(res => res.json())
+        .then((data) => {setLowProductStock(data)})
+        .catch(console.error());
+    })
+
+    const handleClick = (product) => {
+        history.push({pathname:`/product/${product.id}`, state: product});
+    }
 
     return (
         <div>
             <center>
             { localStorage.getItem('authenticated') === 'true' ?
                 <div className="vertical-center container">
-                        <div className="row">
-                            <div className="col">
-                                <div className="jumbotron container">
-                                    <h2> Welcome {localStorage.getItem('user_name')}</h2>
-                                    <h3>Authority: {localStorage.getItem('authorities')}</h3>
-                                </div>
-                            </div>
-                            <div className="col">
-                                <div className="jumbotron container">
-                                    <h2>Products close to reorder point</h2>
-                                </div>
-                            </div>
-                            <div className="w-100"></div>
-                            <div className="col">
-                                <div className="jumbotron container ">
-                                    <h2>User Accounts</h2>
+                    <div className="row">
+                        <div className="col">
+                            <div className="jumbotron container">
+                                <h2>Low Stock</h2>
+                                <div className="table-wrapper-scroll-y my-custom-scrollbar">
+                                <table class="table table-bordered table-striped mb-0">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Stock Level</th>
+                                        <th scope="col">Reorder Point</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
                                     {
-                                        localStorage.getItem('authorities') === 'USER' ? 
-                                        <Button className="form-control" variant="outline-info" onClick={() => setShowNewPasswordModal(true)}>Update My Password</Button>
-                                        :
-                                        <div>
-                                            <Button className="form-control" variant="outline-info" onClick={() => setShowNewPasswordModal(true)}>Update My Password</Button>
-                                            <Button className="form-control mt-1" variant="outline-info" onClick={() => setShowAddUserModal(true)}>New User</Button>
-                                            <Button className="form-control mt-1" variant="warning" onClick={() => setShowEditUserModal(true)}>Edit User</Button>
-                                        </div>
+                                        lowProductStock.length === 0 ? <center><tr><td colspan="4">No products low on stock!</td></tr></center> : 
+                                        lowProductStock.map((product) => {
+                                            return (
+                                                <tr key={product.id} onClick={() => handleClick(product)}>
+                                                    <td>{product.id}</td>
+                                                    <td>{product.name}</td>
+                                                    <td>{product.inventory_on_hand}</td>
+                                                    <td>{product.reorder_threshold}</td>
+                                                </tr>
+                                            )
+                                        })
                                     }
-                                </div>
-                            </div>
-                            <div className="col">
-                                <div className="jumbotron container">
-                                    <h2>Daily Waste</h2>
-                                    <Button className="form-control" variant="outline-info" onClick={() => history.push('/waste')}>Submit Waste Report</Button>
+                                    </tbody>
+                                </table>
                                 </div>
                             </div>
                         </div>
+                        <div className="w-100"></div>
+                        <div className="col">
+                            <div className="jumbotron container h-100">
+                                <h2>User Accounts</h2>
+                                {
+                                    localStorage.getItem('authorities') === 'USER' ? 
+                                    <Button className="form-control" variant="outline-info" onClick={() => setShowNewPasswordModal(true)}>Update My Password</Button>
+                                    :
+                                    <div>
+                                        <Button className="form-control" variant="outline-info" onClick={() => setShowNewPasswordModal(true)}>Update My Password</Button>
+                                        <Button className="form-control mt-1" variant="outline-info" onClick={() => setShowAddUserModal(true)}>New User</Button>
+                                        <Button className="form-control mt-1" variant="warning" onClick={() => setShowEditUserModal(true)}>Edit User</Button>
+                                    </div>
+                                }
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="jumbotron container h-100">
+                                <h2>Daily Waste</h2>
+                                <Button className="form-control" variant="outline-info" onClick={() => history.push('/waste')}>Submit Waste Report</Button>
+                                <h2> Welcome {localStorage.getItem('user_name')} Authority: {localStorage.getItem('authorities')}</h2>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                             :
                 <div className="vertical-center">
